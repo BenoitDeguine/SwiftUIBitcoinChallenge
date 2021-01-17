974ead356ffc2fa65fb7d33b3ab769c75b4f30d6
Benoît Deguine <bdeguine91@gmail.com>
2021-01-17 15:37:42 +0000

    Initial Commit

---
+++ Shared/BitcoinChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitcoinChallengeApp.swift
+//  Shared
+//
+//  Created by Benoit on 17/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct BitcoinChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
